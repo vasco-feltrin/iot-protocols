75a9111a93cc5b39ae3d09d228ad435edde0d03c
Riccardo Casco <riccardo.casco@beantech.it>
2024-04-04 16:56:45 +0200

    aggiunto sensore temperatura

--- Server/NetCoreServer/WebApplication1/Controllers/CarSensorsController.cs
+++ Server/NetCoreServer/WebApplication1/Controllers/CarSensorsController.cs
@@ -10,9 +10,30 @@ namespace WebApplication1.Controllers
         private readonly ILogger<CarSensorsController> _logger = logger;
 
         [HttpPost("{carId}/speed")]
-        public IActionResult SaveSpeedData([FromBody] SpeedSensorModel speed)
+        public IActionResult SaveSpeedData(int carId, [FromBody] SpeedSensorModel speedData)
         {
-            Console.WriteLine( /*carId + */" --- " + speed);
+            Console.WriteLine(carId + " --- " + speedData);
+            return Ok();
+        }
+
+        [HttpPost("{carId}/rotation")]
+        public IActionResult SaveRotationData(int carId, [FromBody] GyroscopeModel rotationData)
+        {
+            Console.WriteLine(carId + " --- " + rotationData);
+            return Ok();
+        }
+
+        [HttpPost("{carId}/position")]
+        public IActionResult SavePositionData(int carId, [FromBody] GpsSensorModel gpsData)
+        {
+            Console.WriteLine($"Position: {carId} --- {gpsData}");
+            return Ok();
+        }
+
+        [HttpPost("{carId}/temperature")]
+        public IActionResult SaveTemperatureData(int carId, [FromBody] EngineTemperatureSensorModel tempData)
+        {
+            Console.WriteLine($"Temperature: {carId} --- {tempData}");
             return Ok();
         }
     }

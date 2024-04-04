8960091776925e283f59bbcf5a85610cf415e5f6
Riccardo Casco <riccardo.casco@beantech.it>
2024-04-04 17:31:23 +0200

    modificato id int -> guid

--- Server/NetCoreServer/WebApplication1/Controllers/CarSensorsController.cs
+++ Server/NetCoreServer/WebApplication1/Controllers/CarSensorsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using WebApplication1.Model;
 
 namespace WebApplication1.Controllers
 {
@@ -10,31 +11,48 @@ namespace WebApplication1.Controllers
         private readonly ILogger<CarSensorsController> _logger = logger;
 
         [HttpPost("{carId}/speed")]
-        public IActionResult SaveSpeedData(int carId, [FromBody] SpeedSensorModel speedData)
+        public IActionResult SaveSpeedData(Guid carId, [FromBody] SpeedSensorModel speedData)
         {
             Console.WriteLine(carId + " --- " + speedData);
             return Ok();
         }
 
         [HttpPost("{carId}/rotation")]
-        public IActionResult SaveRotationData(int carId, [FromBody] GyroscopeModel rotationData)
+        public IActionResult SaveRotationData(Guid carId, [FromBody] GyroscopeModel rotationData)
         {
             Console.WriteLine(carId + " --- " + rotationData);
             return Ok();
         }
 
         [HttpPost("{carId}/position")]
-        public IActionResult SavePositionData(int carId, [FromBody] GpsSensorModel gpsData)
+        public IActionResult SavePositionData(Guid carId, [FromBody] GpsSensorModel gpsData)
         {
             Console.WriteLine($"Position: {carId} --- {gpsData}");
             return Ok();
         }
 
         [HttpPost("{carId}/temperature")]
-        public IActionResult SaveTemperatureData(int carId, [FromBody] TemperatureSensorModel tempData)
+        public IActionResult SaveTemperatureData(Guid carId, [FromBody] TemperatureSensorModel tempData)
         {
             Console.WriteLine($"Temperature: {carId} --- {tempData}");
             return Ok();
+        }
+
+        [HttpGet("{carId}")]
+        public IEnumerable<Car> GetCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Ottine la lista degli Id di tutte le macchine
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        [HttpGet]
+        public IEnumerable<Guid> GetCarList()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Server/NetCoreServer/WebApplication1/Model/Car.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace WebApplication1.Model
+{
+    public class Car
+    {
+        public required Guid Id { get; set; }
+        public required GpsSensorModel Gps { get; set; }
+        public required SpeedSensorModel Speed { get; set; }
+        public required TemperatureSensorModel Temperature { get; set; }
+        public required GyroscopeModel Gyroscope { get; set; }
+    }
+}

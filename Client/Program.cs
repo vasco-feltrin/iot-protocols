2c12e6f44f0623e1bfa325316e148c1f2d15db60
vasco <matteovasco.feltrin@stud.itsaltoadriatico.it>
2024-04-04 15:51:07 +0200

    update gyroscope

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,24 +5,26 @@ using NetCoreClient.Protocols;
 List<ISensor> sensors = new();
 sensors.Add(new VirtualSpeedSensor());
 sensors.Add(new VirtualGpsSensor());
+sensors.Add(new VirtualGpsSensor());
 // define protocol
 ProtocolInterface protocol = new Http("http://localhost:8011/cars/123");
 
 // send data to server
-while (true)
+
+foreach (ISensor sensor in sensors)
 {
-    foreach (ISensor sensor in sensors)
+
+    Task _ = new Task(() =>
     {
+        while (true)
+        {
+            var sensorValue = sensor.ToJson();
 
+            protocol.Send(sensorValue);
 
+            Console.WriteLine("Data sent: " + sensorValue);
 
-        var sensorValue = sensor.ToJson();
-
-        protocol.Send(sensorValue);
-
-        Console.WriteLine("Data sent: " + sensorValue);
-
-        Thread.Sleep(1000);
-    }
-
+            Thread.Sleep(1000);
+        }
+    });
 }

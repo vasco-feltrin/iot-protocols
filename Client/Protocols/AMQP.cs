cce6582f197048e5d94fd5eb18fdf87f331991e4
Rick <rick@me.io>
2024-05-09 10:30:09 +0200

    amqp

--- Client/Protocols/AMQP.cs
+++ Client/Protocols/AMQP.cs
@@ -1,4 +1,6 @@
-﻿namespace NetCoreClient.Protocols;
+﻿using RabbitMQ.Client;
+
+namespace NetCoreClient.Protocols;
 
 public class AmqpProtocolInterface : IProtocolInterface
 {
@@ -6,6 +8,24 @@ public class AmqpProtocolInterface : IProtocolInterface
 
     {
         // send data here
-        throw new NotImplementedException();
+        // Define connection factory
+        ConnectionFactory factory = new()
+        {
+            HostName = "localhost", // RabbitMQ server host
+            Port = 5672,            // RabbitMQ server port
+            UserName = "guest",     // RabbitMQ username
+            Password = "guest"      // RabbitMQ password
+        };
+
+        // Create connection
+        using (var connection = factory.CreateConnection())
+        {
+            // Create channel
+            using (var channel = connection.CreateModel())
+            {
+                // Use the channel for further operations, like creating queues, exchanges, publishing messages, etc.
+                Console.WriteLine("Connected to RabbitMQ!");
+            }
+        }
     }
 }
